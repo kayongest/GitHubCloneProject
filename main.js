1e5544664c69d08566589c046c131e2ddff87e3b
kayongest <kayonga70@gmail.com>
2025-05-19 17:36:12 +0200

    Javascript Fundamentals - Objects.txt

--- main.js
+++ main.js
@@ -3,3 +3,7 @@
 "use strict"; // This is the modern way of using Javascript!
 
 
+// Store a reference to the <h1> in a variable
+const myHeading = document.querySelector("h1");
+// Update the text content of the <h1>
+myHeading.textContent = "Hello world!";
